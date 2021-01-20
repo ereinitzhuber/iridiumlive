a360ae0f04d0fa32c694806f84c884631e89af87
Ethan M Rein <ereinitzhuber@gmail.com>
2021-01-19 22:37:19 -0500

    Replace timestamp and offset from packet with locally generated Unix timestamp.

--- IridiumLive/Services/SatsService.cs
+++ IridiumLive/Services/SatsService.cs
@@ -123,8 +123,8 @@ namespace IridiumLive.Services
             {
                 string newGuid = Guid.NewGuid().ToString();
                 string[] words = rxLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                string[] words1 = words[2].Split('.');
-                DateTimeOffset satTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(words1[0], CultureInfo.InvariantCulture)).AddMilliseconds(Convert.ToInt64(words1[1], CultureInfo.InvariantCulture)).ToLocalTime();
+                long currentstamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+                DateTimeOffset satTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(currentstamp, CultureInfo.InvariantCulture)).ToLocalTime();
                 long utcTicks = satTime.ToUniversalTime().UtcTicks;
                 int quality = Convert.ToInt32(words[4].TrimEnd('%'), CultureInfo.InvariantCulture);
                 int satNo;
